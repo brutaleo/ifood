a96e3aca408b21ca4af2838714ef2295e6a77681
Leonardo Pereira Rodrigues <brutaleo@hotmail.com>
2021-10-19 01:50:28 -0300

    Criação do Teste para o Endpoint PUT
    - Utilizando o cenário restaurantes-cenario-1.yml.

--- cadastro/src/test/java/com/github/brutaleo/ifood/RestauranteResourceTest.java
+++ cadastro/src/test/java/com/github/brutaleo/ifood/RestauranteResourceTest.java
@@ -1,15 +1,20 @@
 package com.github.brutaleo.ifood;
 
+import com.github.brutaleo.ifood.cadastro.model.Restaurante;
 import com.github.database.rider.cdi.api.DBRider;
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.configuration.Orthography;
 import com.github.database.rider.core.api.dataset.DataSet;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import org.approvaltests.Approvals;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
+import javax.ws.rs.core.Response;
 
 @DBRider
 @DBUnit(caseInsensitiveStrategy = Orthography.LOWERCASE)
@@ -20,7 +25,7 @@ public class RestauranteResourceTest {
 
     @Test
     @DataSet("restaurantes-cenario-1.yml")
-    public void RestaurantesEndpointTest() {
+    public void testandoSeOEndpointGetRetornaStatusCode200NoCenarioDefinidoNoDataset() {
         String resultado = given()
                 .when().get("/restaurantes")
                 .then()
@@ -28,6 +33,33 @@ public class RestauranteResourceTest {
                 .extract()
                 .asString();
         Approvals.verifyJson(resultado);
+    }
+
+    //utilizar o given do RestAssured sem definir o tipo de conteúdo gera um erro.
+    private RequestSpecification given() {
+        return RestAssured.given().contentType(ContentType.JSON); //Definição do método given() para receber o conteúdo JSON.
+    }
+
+    @Test
+    @DataSet("restaurantes-cenario-1.yml")
+    public void testandoSeOEndpointPutAlteraUmRestauranteNoCenarioDefinidoNoDataset() {
+        Restaurante dto = new Restaurante();
+        dto.nome = "Novo-Nome";
+        Long parameterValue = 123L;
+        given()
+                .with()
+                .pathParam("id", parameterValue)
+                .body(dto)
+                .when()
+                .put("/restaurantes/{id}")
+                .then()
+                .statusCode(Response.Status.NO_CONTENT.getStatusCode()) //não possui resposta para exibir
+                .extract()
+                .asString();
+
+        Restaurante findById = Restaurante.findById(parameterValue); //consulta no banco de dados para verificar se a alteração foi realizada
+
+        Assertions.assertEquals(dto.nome, findById.nome); //assertion que verifica se a alteração
     }
 
 }
