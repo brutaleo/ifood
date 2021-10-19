4676d81d291885f8599eb342947d8c82d6285d57
Leonardo Pereira Rodrigues <brutaleo@hotmail.com>
2021-10-19 01:05:58 -0300

    Criação do Cenário de testes para Restaurantes
    - Criação do arquivo restaurantes-cenario-1.yml com os dados para serem populados no banco de dados;
    - Ferramenta utilizada DBRider.

--- cadastro/src/test/java/com/github/brutaleo/ifood/RestauranteResourceTest.java
+++ cadastro/src/test/java/com/github/brutaleo/ifood/RestauranteResourceTest.java
@@ -1,16 +1,25 @@
 package com.github.brutaleo.ifood;
 
+import com.github.database.rider.cdi.api.DBRider;
+import com.github.database.rider.core.api.configuration.DBUnit;
+import com.github.database.rider.core.api.configuration.Orthography;
+import com.github.database.rider.core.api.dataset.DataSet;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
+@DBRider
+@DBUnit(caseInsensitiveStrategy = Orthography.LOWERCASE)
+// previne as checagens do postgres vs as definições no arquivo *.yml
 @QuarkusTest
 @QuarkusTestResource(CadastroTestLifeCycleManager.class)
 public class RestauranteResourceTest {
 
     @Test
+    @DataSet("restaurantes-cenario-1.yml")
     public void RestaurantesEndpointTest() {
         String resultado = given()
                 .when().get("/restaurantes")
@@ -18,7 +27,7 @@ public class RestauranteResourceTest {
                 .statusCode(200)
                 .extract()
                 .asString();
-        //Approvals.verifyJson(resultado);
+        Approvals.verifyJson(resultado);
     }
 
 }
