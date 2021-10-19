d37bc40826d7264dac6f9471ceea3f47fbabd8c2
Leonardo Pereira Rodrigues <brutaleo@hotmail.com>
2021-10-19 19:55:31 -0300

    Adicionada proteção dos Endpoints e atualizado o Open API
    - Instalada a extensão smallrye-JWT
    - Regras de permissão adicionada em RestaurantesResource a nível de classe;
    - Adicionado no application.properties a chave publica do token para o usuário "proprietario"

--- cadastro/src/main/java/com/github/brutaleo/ifood/cadastro/resource/RestauranteResource.java
+++ cadastro/src/main/java/com/github/brutaleo/ifood/cadastro/resource/RestauranteResource.java
@@ -11,11 +11,17 @@ import com.github.brutaleo.ifood.cadastro.dto.RestauranteMapper;
 import com.github.brutaleo.ifood.cadastro.infra.ConstraintViolationResponse;
 import com.github.brutaleo.ifood.cadastro.model.Prato;
 import com.github.brutaleo.ifood.cadastro.model.Restaurante;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlow;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlows;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
@@ -41,6 +47,16 @@ import java.util.stream.Stream;
 @Path("/restaurantes")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@RolesAllowed("proprietario")
+@SecurityScheme(
+        securitySchemeName = "ifood-oauth",
+        type = SecuritySchemeType.OAUTH2,
+        flows = @OAuthFlows(
+                password = @OAuthFlow(
+                        tokenUrl = "http://localhost:8280/auth/realms/ifood/protocol/openid-connect/token")
+        )
+)
+@SecurityRequirement(name = "ifood-oauth", scopes = {})
 public class RestauranteResource {
 
     @Inject
